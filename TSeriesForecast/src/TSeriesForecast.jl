d49ab630831b227857626a16fe3850bdd5d001d0
Mariano Nicolini <mariano.nicolini@lambdaclass.com>
2020-12-21 11:31:23 -0300

    created project template

---
+++ TSeriesForecast/src/TSeriesForecast.jl
@@ -0,0 +1,5 @@
+module TSeriesForecast
+
+# Write your package code here.
+
+end
---
+++ TSeriesForecast/test/runtests.jl
@@ -0,0 +1,6 @@
+using TSeriesForecast
+using Test
+
+@testset "TSeriesForecast.jl" begin
+    # Write your tests here.
+end
